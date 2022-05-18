a7dd53bef56c15f147a2f6c5f8921aa426427d19
Soufiane Essaouari <soufiane.pro@yahoo.com>
2022-05-18 02:08:48 +0200

    Initial commit

---
+++ MVC-POO-PHP-FromScratch-main/modele/GameManager.php
@@ -0,0 +1,55 @@
+<?php
+ require_once "Manager.php";
+ require_once "Game.php";
+
+class GameManager extends Manager {
+
+    private $games;
+
+    public function addGame($game){
+        $this->games[] = $game;
+    }
+
+    public function getGames(){
+        return $this->games;
+    }
+
+    public function getGameById($id){
+        foreach($this->games as $game) {
+            if ($id == $game->getId()) {
+                return $game;
+            }
+        }
+    }
+
+    public function loadGames(){
+        $req = $this->getBdd()->prepare("SELECT * FROM conducteur");
+        $req->execute();
+        $myGames = $req->fetchAll(PDO::FETCH_ASSOC);
+        $req->closeCursor();
+
+        foreach($myGames as $game){
+            $g = new Game($game['id_conducteur'],$game['prenom'],$game['nom']);
+            $this->addGame($g);
+        }
+
+    }
+
+    public function newGameDB($title,$nbPlayers){
+        // $req = "INSERT INTO games (title, nb_players) VALUES (:title, :nbPlayers)";
+        $req = "INSERT INTO games (prenom, nom) VALUES (:prenom, :nom)";
+        $statement = $this->getBdd()->prepare($req);
+        $statement->bindValue(":prenom",$title, PDO::PARAM_STR);
+        $statement->bindValue(":nom",$nbPlayers, PDO::PARAM_INT);
+        $result = $statement->execute();
+        $statement->closeCursor();
+
+        if ($result) {
+            $g = new Game($this->getBdd()->lastInsertId(),$title,$nbPlayers);
+            $this->addGame($g);
+        }
+        
+    }
+
+
+}
